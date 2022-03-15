50cd658d446d3992189dbd1a31e677d48ce87f7b
Ismail Askhatir <spxoki@gmail.com>
2022-03-15 11:02:02 +0100

    modif win message

--- script.js
+++ script.js
@@ -17,83 +17,96 @@ const case8 = document.querySelector("body > main > section.container > div:nth-
 
 
 
+
+
 function colors() {
-  
 }
+
+
 
 function checkWin() {
 
   if (case0.innerHTML == 'X' && case1.innerHTML == 'X' && case2.innerHTML == 'X') {
    alert('Player X Has Won!')
-   result.innerHTML = 'Congratulation Player X !'
-  } else   if (case0.innerHTML == 'O' && case1.innerHTML == 'O' && case2.innerHTML == 'O') {
+   result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
+  } 
+  else   if (case0.innerHTML == 'O' && case1.innerHTML == 'O' && case2.innerHTML == 'O') {
     alert('Player O Has Won!')
-    result.innerHTML = 'Congratulation Player O !'
-   } else if (case3.innerHTML == 'X' && case4.innerHTML == 'X' && case5.innerHTML == 'X') {
+    result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+   } 
+   else if (case3.innerHTML == 'X' && case4.innerHTML == 'X' && case5.innerHTML == 'X') {
     alert('Player X Has Won!')
-    result.innerHTML = 'Congratulation Player X !'
-   } else if (case3.innerHTML == 'O' && case4.innerHTML == 'O' && case5.innerHTML == 'O') {
+    result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
+   } 
+   else if (case3.innerHTML == 'O' && case4.innerHTML == 'O' && case5.innerHTML == 'O') {
     alert('Player O Has Won!')
-    result.innerHTML = 'Congratulation Player O !'
-} else if (case6.innerHTML == 'O' && case7.innerHTML == 'O' && case8.innerHTML == 'O') {
+    result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+} 
+else if (case6.innerHTML == 'O' && case7.innerHTML == 'O' && case8.innerHTML == 'O') {
   alert('Player O Has Won!') 
-  result.innerHTML = 'Congratulation Player O !'
-} else if (case6.innerHTML == 'X' && case7.innerHTML == 'X' && case8.innerHTML == 'X') {
+  result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+} 
+else if (case6.innerHTML == 'X' && case7.innerHTML == 'X' && case8.innerHTML == 'X') {
   alert('Player X Has Won!')
-  result.innerHTML = 'Congratulation Player X !'
- } else if (case0.innerHTML == 'X' && case4.innerHTML == 'X' && case8.innerHTML == 'X') {
-  alert('Player X Has Won!')
-  result.innerHTML = 'Congratulation Player X !'
- } else if (case0.innerHTML == 'O' && case4.innerHTML == 'O' && case8.innerHTML == 'O') {
-  alert('Player O Has Won!')
-  result.innerHTML = 'Congratulation Player O !'
- } else if (case2.innerHTML == 'O' && case4.innerHTML == 'O' && case6.innerHTML == 'O') {
-  alert('Player O Has Won!')
-  result.innerHTML = 'Congratulation Player O !'
- } else if (case2.innerHTML == 'X' && case4.innerHTML == 'X' && case6.innerHTML == 'X') {
-  alert('Player X Has Won!')
-  result.innerHTML = 'Congratulation Player X !'
- } else if (case0.innerHTML == 'X' && case3.innerHTML == 'X' && case6.innerHTML == 'X') {
-  alert('Player X Has Won!')
-  result.innerHTML = 'Congratulation Player X !'
- } else if (case0.innerHTML == 'O' && case3.innerHTML == 'O' && case6.innerHTML == 'O') {
-  alert('Player O Has Won!')
-  result.innerHTML = 'Congratulation Player O !'
- } else if (case1.innerHTML == 'X' && case4.innerHTML == 'X' && case7.innerHTML == 'X') {
-  alert('Player X Has Won!')
-  result.innerHTML = 'Congratulation Player X !'
- } else if (case1.innerHTML == 'O' && case4.innerHTML == 'O' && case7.innerHTML == 'O') {
-  alert('Player O Has Won!')
-  result.innerHTML = 'Congratulation Player O !'
- } else if (case2.innerHTML == 'X' && case5.innerHTML == 'X' && case8.innerHTML == 'X') {
-  alert('Player X Has Won!')
-  result.innerHTML = 'Congratulation Player X !'
- } else if (case2.innerHTML == 'O' && case5.innerHTML == 'O' && case8.innerHTML == 'O') {
-  alert('Player O Has Won!')
-  result.innerHTML = 'Congratulation Player O !'
+  result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
  } 
- 
+ else if (case0.innerHTML == 'X' && case4.innerHTML == 'X' && case8.innerHTML == 'X') {
+  alert('Player X Has Won!')
+  result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
+ }
+  else if (case0.innerHTML == 'O' && case4.innerHTML == 'O' && case8.innerHTML == 'O') {
+  alert('Player O Has Won!')
+  result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case2.innerHTML == 'O' && case4.innerHTML == 'O' && case6.innerHTML == 'O') {
+  alert('Player O Has Won!')
+  result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case2.innerHTML == 'X' && case4.innerHTML == 'X' && case6.innerHTML == 'X') {
+  alert('Player X Has Won!')
+  result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case0.innerHTML == 'X' && case3.innerHTML == 'X' && case6.innerHTML == 'X') {
+  alert('Player X Has Won!')
+  result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case0.innerHTML == 'O' && case3.innerHTML == 'O' && case6.innerHTML == 'O') {
+  alert('Player O Has Won!')
+  result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case1.innerHTML == 'X' && case4.innerHTML == 'X' && case7.innerHTML == 'X') {
+  alert('Player X Has Won!')
+  result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case1.innerHTML == 'O' && case4.innerHTML == 'O' && case7.innerHTML == 'O') {
+  alert('Player O Has Won!')
+  result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case2.innerHTML == 'X' && case5.innerHTML == 'X' && case8.innerHTML == 'X') {
+  alert('Player X Has Won!')
+  result.innerHTML = 'Congratulation Player X ! <br> Click The Reset Button Below to Try Again =)'
+ } 
+ else if (case2.innerHTML == 'O' && case5.innerHTML == 'O' && case8.innerHTML == 'O') {
+  alert('Player O Has Won!')
+  result.innerHTML = 'Congratulation Player O ! <br> Click The Reset Button Below to Try Again =)'
+ } 
 }
-
 
 
 gameBoard.addEventListener('click', colors, false)
   function colors(event) {
    if (playerX.innerHTML == 'X') {
     event.target.innerHTML = 'X'
-    playerX.innerHTML = 'O'
-    checkWin()
-  
+      playerX.innerHTML = 'O'
+        checkWin()
    } else if (event.target.innerHTML) {
-     return false
- 
+        return false
    }
     else {
     event.target.innerHTML = 'O'
-    playerX.innerHTML = 'X'
-    checkWin()
+      playerX.innerHTML = 'X'
+        checkWin()
    }
- 
   }
 
 resetBtn.addEventListener('click', function() {
